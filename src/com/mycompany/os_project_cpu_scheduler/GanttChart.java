0c29fa56dc1a0a95ffb3fb6b2006e2af4f9f099e
Takucan <Takucan@WIN-Q47ITF59IRK>
2025-07-16 18:34:42 +0800

    The file is added

--- src/com/mycompany/os_project_cpu_scheduler/GanttChart.java
+++ src/com/mycompany/os_project_cpu_scheduler/GanttChart.java
@@ -8,6 +8,26 @@ package com.mycompany.os_project_cpu_scheduler;
  *
  * @author Aledon & Tipactipac
  */
+import java.util.*;
+
 public class GanttChart {
-    
-}
+    public static class Interval {
+        String processId;
+        int startTime;
+        int endTime;
+        String annotation; // e.g., "(Q0)" for MLFQ, or "" for others
+
+        public Interval(String processId, int startTime, String annotation) {
+            this.processId = processId;
+            this.startTime = startTime;
+            this.annotation = annotation == null ? "" : annotation;
+        }
+
+        public void setEndTime(int endTime) {
+            this.endTime = endTime;
+        }
+
+        public String getLabel() {
+            return processId + annotation;
+        }
+    }
