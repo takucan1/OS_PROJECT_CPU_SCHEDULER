3b03b0c2592c21998b9e98b1480335a32faaa480
Takucan <Takucan@WIN-Q47ITF59IRK>
2025-07-16 20:17:26 +0800

    The file is added

--- src/com/mycompany/os_project_cpu_scheduler/FCFS.java
+++ src/com/mycompany/os_project_cpu_scheduler/FCFS.java
@@ -31,3 +31,42 @@ public class FCFS implements Scheduler {
             ganttChart.add("| " + p.id + " ");
         }
     }
+
+    @Override
+    public List<String> getGanttChart() {
+        return ganttChart;
+    }
+
+    @Override
+    public void printMetrics(List<Process> processes) {
+        System.out.printf("%-10s%-15s%-15s%-15s%-20s%-15s\n",
+                "Process", "Arrival Time", "Burst Time", "Completion", "Turnaround Time", "Response Time");
+        for (Process p : processes) {
+            System.out.printf("%-10s%-15d%-15d%-15d%-20d%-15d\n",
+                    p.id, p.arrivalTime, p.burstTime, p.completionTime, p.turnaroundTime, p.responseTime);
+        }
+
+        System.out.println("\nAverage Turnaround Time: " + getAverageTurnaroundTime(processes));
+        System.out.println("Average Response Time: " + getAverageResponseTime(processes));
+    }
+
+    @Override
+    public double getAverageTurnaroundTime(List<Process> processes) {
+        int total = 0;
+        for (Process p : processes) {
+            total += p.turnaroundTime;
+        }
+        return (double) total / processes.size();
+    }
+
+    @Override
+    public double getAverageResponseTime(List<Process> processes) {
+        int total = 0;
+        for (Process p : processes) {
+            total += p.responseTime;
+        }
+        return (double) total / processes.size();
+    }
+}
+
+
